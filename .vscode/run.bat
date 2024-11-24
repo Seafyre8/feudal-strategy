1be794fc0b5692cf011fcd581e2fc94cb8264f18
Joseph <joseph.p.baca8@gmail.com>
2024-11-23 16:41:50 -0800

    Delete .vscode/run.bat

--- .vscode/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH_TO_FX="%~dp0lib"
-javac --module-path %PATH_TO_FX% --add-modules=javafx.base,javafx.graphics,javafx.controls Main.java
-java --module-path %PATH_TO_FX% --add-modules=javafx.base,javafx.graphics,javafx.controls Main
-pause
